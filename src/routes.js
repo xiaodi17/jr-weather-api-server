ccd2eedaaffd9c740a40f33bd21dec45adff012d
David <xiaodi.david@gmail.com>
2019-02-07 21:23:50 +1000

    add response formatter

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const weatherRoutes = require("./routes/weather");
-
+const responseFormatter = require("./utils/responseFormatter");
 //provided by express
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("welcome");
-});
+router.get("/", (req, res) =>
+  responseFormatter(
+    res,
+    200,
+    "Welcome to the weather API Visit /api-docs for help",
+    null
+  )
+);
 
 router.use("/api/weather", weatherRoutes);
 
