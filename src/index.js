e4eed69a4567980033867ab14d808935597a71de
David <xiaodi.david@gmail.com>
2019-02-07 22:00:51 +1000

    add cors

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 const routes = require("./routes");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 const logger = require("./utils/logger");
 const notFoundHandler = require("./middleware/notFound");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 app.use(helmet());
+app.use(cors());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 } else {
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -2,10 +2,11 @@ const express = require("express");
 const axios = require("../utils/axios");
 const weather = require("../models/Weather");
 const responseFormatter = require("../utils/responseFormatter");
+const countryValidator = require("../middleware/countryValidator");
 const router = express.Router();
 const APPID = process.env.APPID;
 
-router.get("/:cc/:city", (req, res, next) => {
+router.get("/:cc/:city", countryValidator, (req, res, next) => {
   // res.send('weather');
   const { cc, city } = req.params;
   const weatherType = req.query.weatherType;
