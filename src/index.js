c3bfbdc086e0f6effe6f82c1c2bf8f08dec6a660
David <xiaodi.david@gmail.com>
2019-02-07 21:43:49 +1000

    add error handling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const logger = require("./utils/logger");
 const notFoundHandler = require("./middleware/notFound");
-
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(helmet());
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(routes);
+app.use(errorHandler);
 app.use(notFoundHandler);
 
 app.listen(PORT, () => logger.info(`app listen on port ${PORT}`));
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -5,14 +5,14 @@ const responseFormatter = require("../utils/responseFormatter");
 const router = express.Router();
 const APPID = process.env.APPID;
 
-router.get("/:cc/:city", (req, res) => {
+router.get("/:cc/:city", (req, res, next) => {
   // res.send('weather');
   const { cc, city } = req.params;
   const weatherType = req.query.weatherType;
   weather
     .getData(city, cc, weatherType)
     .then(response => responseFormatter(res, 200, null, response))
-    .catch(err => console.log(err));
+    .catch(error => next(error));
 });
 
 module.exports = router;
